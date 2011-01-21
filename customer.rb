cb2e5cbd25874d14f64a7ef90cd46aad87c94efc
Tatsuro BABA <t.baba@grooves.co.jp>
2011-01-21 19:25:03 +0900

    each -> inject

--- customer.rb
+++ customer.rb
@@ -27,10 +27,6 @@ class Customer
 
   private
   def total_charge
-    result = 0
-    @rentals.each do |element|
-      result += element.charge
-    end
-    result
+    @rentals.inject(0) {|sum, rental| sum + rental.charge }
   end
 end
