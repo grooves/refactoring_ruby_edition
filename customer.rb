21a33a1c5dcc98e5bf229a9b544abcb35fabe982
Tatsuro BABA <t.baba@grooves.co.jp>
2011-01-21 19:20:27 +0900

    removing temps

--- customer.rb
+++ customer.rb
@@ -11,18 +11,26 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
       frequent_renter_points += element.frequent_renter_points
 
       # このレンタルの料金を表示
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
     end
     # フッター行を追加
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{total_charge}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
+    result
+  end
+
+  private
+  def total_charge
+    result = 0
+    @rentals.each do |element|
+      result += element.charge
+    end
     result
   end
 end
