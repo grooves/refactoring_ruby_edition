d17508a829d007ba97e477215f7061bdf7761941
Tatsuro BABA <t.baba@grooves.co.jp>
2011-01-21 19:14:43 +0900

    テスト用のスクリプト

---
+++ test.rb
@@ -0,0 +1,17 @@
+require 'customer'
+require 'rental'
+require 'movie'
+
+baba = Customer.new 'baba'
+mov1 = Movie.new 'movie1', Movie::REGULAR
+mov2 = Movie.new 'movie2', Movie::NEW_RELEASE
+mov3 = Movie.new 'movie3', Movie::CHILDRENS
+rental1 = Rental.new mov1, 7
+rental2 = Rental.new mov2, 3
+rental3 = Rental.new mov3, 10
+
+baba.add_rental rental1
+baba.add_rental rental2
+baba.add_rental rental3
+
+puts baba.statement
