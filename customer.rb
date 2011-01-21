ed78aea31286055e73985b2c8f4797344d535683
Tatsuro BABA <t.baba@grooves.co.jp>
2011-01-21 19:34:00 +0900

    add method #total_frequent_renter_points

--- customer.rb
+++ customer.rb
@@ -11,22 +11,22 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-
-      # このレンタルの料金を表示
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
     # フッター行を追加
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
   private
   def total_charge
     @rentals.inject(0) {|sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) {|sum, rental| sum + rental.frequent_renter_points }
   end
 end
