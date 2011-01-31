c14a6f7caaf3d6f043bc1c9b97704512aaa82281
personlink <admin@personlink.jp>
2011-01-31 20:51:09 +0900

    1.7 料金コードによる条件分岐からポリモーフィズムへ

--- movie.rb
+++ movie.rb
@@ -9,4 +9,24 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+  
+  def charge(days_rented)
+    result = 0
+    # 各行の金額を計算
+    case price_code
+    when REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      result += days_rented * 3
+    when CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+  
+  def frequent_renter_points(days_rented)
+    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 end
--- rental.rb
+++ rental.rb
@@ -4,24 +4,12 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
-
+  
   def charge
-    result = 0
-    # 各行の金額を計算
-    case movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    movie.charge(days_rented)
   end
-
+  
   def frequent_renter_points
-    (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    movie.frequent_renter_points(days_rented)
   end
 end
