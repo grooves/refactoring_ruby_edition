3627c015c3086b099f8a4c9a6a77120cbc7cffaf
personlink <admin@personlink.jp>
2011-01-31 21:26:17 +0900

    1.8 ついに継承導入か その1

--- movie.rb
+++ movie.rb
@@ -3,30 +3,47 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
-  
+
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+             when REGULAR: RegularPrice.new
+             when NEW_RELEASE: NewReleasePrice.new
+             when CHILDRENS: ChildrensPrice.new
+             end
+  end
+
   def charge(days_rented)
-    result = 0
-    # 各行の金額を計算
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
-  
+
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
+
+  class RegularPrice
+    def charge(days_rented)
+      result = 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+      result
+    end
+  end
+
+  class NewReleasePrice
+    def charge(days_rented)
+      result = days_rented * 3
+    end
+  end
+  
+  class ChildrensPrice
+    def charge(days_rented)
+      result = 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
   end
 end
