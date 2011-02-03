2a8eeaac9258aa5915d16bb0e448d92ddc1dfc9b
chin <s.chin@grooves.co.jp>
2011-02-03 21:50:16 +0900

    add testcode for sample

--- chapter6/ReplaceTempwithQuery/sample.rb
+++ chapter6/ReplaceTempwithQuery/sample.rb
@@ -1,5 +1,10 @@
-class sample
+class Sample
+  def initialize(quantity, item_price)
+    @quantity = quantity
+    @item_price = item_price
+  end
   def price
+   #add test data
    base_price = @quantity * @item_price
    if base_price > 1000
      discount_factor = 0.95
---
+++ chapter6/ReplaceTempwithQuery/test.rb
@@ -0,0 +1,6 @@
+require 'sample'
+
+s1 = Sample.new 2, 300
+s2 = Sample.new 3, 400
+puts "S1: #{s1.price}"
+puts "S2: #{s2.price}"
