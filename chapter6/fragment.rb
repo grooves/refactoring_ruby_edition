3598302c3db42dec09ca6388c8aa973800958ae0
chin <s.chin@grooves.co.jp>
2011-02-03 19:58:03 +0900

    using extract method to refactor fragment

--- chapter6/fragment.rb
+++ chapter6/fragment.rb
@@ -1,6 +1,10 @@
 class ExtractMethod
   def print_owing(amount)
     print_banner
+    print_details(amount)
+  end
+
+  def print_details(amount)
     puts "name:#{@name}"
     puts "amount:#{@amount}"
   end
