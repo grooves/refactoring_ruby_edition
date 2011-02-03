9817bdee4f8ef5df49eeb32631da4fdc14a10aad
chin <s.chin@grooves.co.jp>
2011-02-03 21:51:15 +0900

    replace temp base_price

--- chapter6/ReplaceTempwithQuery/sample.rb
+++ chapter6/ReplaceTempwithQuery/sample.rb
@@ -5,12 +5,15 @@ class Sample
   end
   def price
    #add test data
-   base_price = @quantity * @item_price
    if base_price > 1000
      discount_factor = 0.95
    else
      discount_factor = 0.98
    end
    base_price * discount_factor
+  end
+
+  def base_price
+   @quantity * @item_price
   end
 end
