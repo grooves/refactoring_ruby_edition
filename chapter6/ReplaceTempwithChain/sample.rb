1726bc3c4d30d3cc8b357c4f35393c8de1e80c6a
chin <s.chin@grooves.co.jp>
2011-02-04 17:57:50 +0900

    add select instance

--- chapter6/ReplaceTempwithChain/sample.rb
+++ chapter6/ReplaceTempwithChain/sample.rb
@@ -1,9 +1,16 @@
 class Select
-  def options
-    @options ||= []
+  def self.with_option(option)
+    select = self.new
+    select.options << option
+    select
   end
 
+  def options
+   @options ||= []
+  end
+
+
   def add_option(arg)
-    options << arg
+   options << arg
   end
 end
--- chapter6/ReplaceTempwithChain/test.rb
+++ chapter6/ReplaceTempwithChain/test.rb
@@ -1,7 +1,6 @@
 require 'sample'
 
-select = Select.new
-select.add_option(1999)
+select = Select.with_option(1999)
 select.add_option(2000)
 select.add_option(2001)
 select.add_option(2002)
