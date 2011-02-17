2a5e3f01a681d1cefd6ae4d20e156619546b6c82
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-17 13:45:40 +0900

    temporary variable of base price

--- chapter6/IntroduceExplainingVariable/sample.rb
+++ chapter6/IntroduceExplainingVariable/sample.rb
@@ -1,6 +1,7 @@
 def price
   # price is: (base price) - (quantity discount) + (shipping)
-  return @quantity * @item_price - 
+  base_price = @quantity * @item_price
+  return base_price -
     [0, @quantity - 500].max * @item_price * 0.05 +
-    [@quantity * @item_price * 0.1, 100.0].min
+    [base_price * 0.1, 100.0].min
 end
