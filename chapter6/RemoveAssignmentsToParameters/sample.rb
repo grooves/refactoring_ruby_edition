e81b03dfe9422f6e39c821545723492e6f27701c
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-18 08:54:33 +0900

    use temporary variable

--- chapter6/RemoveAssignmentsToParameters/sample.rb
+++ chapter6/RemoveAssignmentsToParameters/sample.rb
@@ -2,9 +2,10 @@ class Sample
   attr_accessor :input_val, :quantity, :year_to_date
 
   def discount(input_val, quantity, year_to_date)
-    input_val -= 2 if input_val > 50
-    input_val -= 1 if quantity > 100
-    input_val -= 4 if year_to_date > 10000
-    input_val
+    result = input_val
+    result -= 2 if input_val > 50
+    result -= 1 if quantity > 100
+    result -= 4 if year_to_date > 10000
+    result
   end
 end
