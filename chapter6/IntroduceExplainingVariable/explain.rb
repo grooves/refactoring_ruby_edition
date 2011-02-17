c7e8d53cebad0d499f75ebd4c257a61067406fdb
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-17 13:36:01 +0900

    temporary variables to explain

--- chapter6/IntroduceExplainingVariable/explain.rb
+++ chapter6/IntroduceExplainingVariable/explain.rb
@@ -1,7 +1,6 @@
-if (platform.upcase.index("MAC") &&
-    browser.upcase.index("IE") &&
-    initialized? &&
-    resize > 0
-   )
+is_mac_os = platform.upcase.index("MAC")
+is_ie = browser.upcase.index("IE")
+was_resized = resize > 0
+if (is_mac_os && is_ie && initialized? && was_resized)
  # do something
 end
