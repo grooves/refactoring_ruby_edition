d87fdee7a8eb92644300c680a237e2ed4fbdf90d
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-18 08:42:58 +0900

    change name from acc to secondary_acc which related @secondary_force

--- chapter6/SplitTemporaryVariable/sample.rb
+++ chapter6/SplitTemporaryVariable/sample.rb
@@ -13,8 +13,8 @@ class Sample
     secondary_time = time - @delay
     if(secondary_time > 0)
       primary_vel = primary_acc * @delay
-      acc = (@primary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + 5 * acc * secondary_time * secondary_time
+      secondary_acc = (@primary_force + @secondary_force) / @mass
+      result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
     end
     result
   end
