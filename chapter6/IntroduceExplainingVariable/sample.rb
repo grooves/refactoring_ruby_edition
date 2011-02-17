7c282aec95355a749210db302e6015e39dd5763b
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-17 21:04:48 +0900

    temporary variables for shipping

--- chapter6/IntroduceExplainingVariable/sample.rb
+++ chapter6/IntroduceExplainingVariable/sample.rb
@@ -4,10 +4,9 @@ class Sample
   end
 
   def price
-    # price is: (base price) - (quantity discount) + (shipping)
     base_price = @quantity * @item_price
     quantity_discount = [0, @quanity - 500].max * @item_price * 0.05
-    return base_price - quantity_discount +
-      [base_price * 0.1, 100.0].min
+    shipping = [base_price * 0.1, 100.0].min
+    return base_price - quantity_discount + shipping
   end
 end
