daf4f92a055d8f875fe0a7b799b46337bd402586
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-17 20:59:56 +0900

    implement class

--- chapter6/IntroduceExplainingVariable/sample.rb
+++ chapter6/IntroduceExplainingVariable/sample.rb
@@ -1,7 +1,13 @@
-def price
-  # price is: (base price) - (quantity discount) + (shipping)
-  base_price = @quantity * @item_price
-  return base_price -
-    [0, @quantity - 500].max * @item_price * 0.05 +
-    [base_price * 0.1, 100.0].min
+class Sample
+  def initialize(quantity, item_price)
+    @quantity, @item_price = quantity, item_price
+  end
+
+  def price
+    # price is: (base price) - (quantity discount) + (shipping)
+    base_price = @quantity * @item_price
+    return base_price -
+      [0, @quantity - 500].max * @item_price * 0.05 +
+      [base_price * 0.1, 100.0].min
+  end
 end
