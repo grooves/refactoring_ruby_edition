e72501c793a53c1a2f811a95681dcbd4e4057836
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-21 20:22:04 +0900

    extract method and to be object

--- chapter6/ReplaceMethodWithMethodObject/gamma.rb
+++ chapter6/ReplaceMethodWithMethodObject/gamma.rb
@@ -13,4 +13,15 @@ class Gamma
     @quantity = quantity_arg
     @year_to_date = year_to_date_arg
   end
+
+  def compute
+    @important_value1 = (input_val * quantity) + @account.delta
+    @important_value2 = (input_val * year_to_date) + 100
+    if (year_to_date - @important_value1) > 100
+      @important_value2 -= 20
+    end
+    @important_value3 = @important_value2 * 7
+    # and some actions
+    @important_value3 - 2 * @important_value1
+  end
 end
--- chapter6/ReplaceMethodWithMethodObject/sample.rb
+++ chapter6/ReplaceMethodWithMethodObject/sample.rb
@@ -1,14 +1,7 @@
 class Account
 
   def gamma(input_val, quantity, year_to_date)
-    important_value1 = (input_val * quantity) + delta
-    important_value2 = (input_val * year_to_date) + 100
-    if (year_to_date - important_value1) > 100
-      important_value2 -= 20
-    end
-    important_value3 = important_value2 * 7
-    # etc
-    important_value3 - 2 * important_value1
+    Gamma.new(self, input_val, quantity, year_to_date).compute
   end
 
 end
