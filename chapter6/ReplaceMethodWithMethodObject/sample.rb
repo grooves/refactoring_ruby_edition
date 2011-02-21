36c1da80005b1febb004aa9ab091b0fbbf60ec81
Tatsuro BABA <t.baba@grooves.co.jp>
2011-02-21 20:22:43 +0900

    require gamma and initialize delta for testing

--- chapter6/ReplaceMethodWithMethodObject/sample.rb
+++ chapter6/ReplaceMethodWithMethodObject/sample.rb
@@ -1,4 +1,10 @@
+require 'gamma'
 class Account
+  attr_accessor :delta
+
+  def initialize(delta)
+    @delta = delta
+  end
 
   def gamma(input_val, quantity, year_to_date)
     Gamma.new(self, input_val, quantity, year_to_date).compute
