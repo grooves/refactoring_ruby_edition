92a7a9fd1f0f0b6ac838dac94c5fa87551439078
personlink <admin@personlink.jp>
2011-02-24 19:07:02 +0900

    6.12 その1　OK

--- ExtractSurroundingMethod/example.rb
+++ ExtractSurroundingMethod/example.rb
@@ -1,7 +1,13 @@
 def charge(amount, credit_card_number)
+  connect do | connection | 
+    connection.send(amount, credit_card_number)
+  end
+end
+
+def connect
   begin
     connection = CreditCardServer.connect(...)
-    connection.send(amount, credit_card_number)
+    yield connection
   rescue IOError => e
     Logger.log "Could not submit order #{@order_number} to the server: #{e}"
     return nil
