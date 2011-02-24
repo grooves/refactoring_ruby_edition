7c22139df06864e85db88bba92664d1027507a50
personlink <admin@personlink.jp>
2011-02-24 18:51:19 +0900

    6.12 その1　NG

---
+++ ExtractSurroundingMethod/example.rb
@@ -0,0 +1,11 @@
+def charge(amount, credit_card_number)
+  begin
+    connection = CreditCardServer.connect(...)
+    connection.send(amount, credit_card_number)
+  rescue IOError => e
+    Logger.log "Could not submit order #{@order_number} to the server: #{e}"
+    return nil
+  ensure
+    connection.close
+  end
+end
