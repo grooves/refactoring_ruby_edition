609d946bb8f3dc3329b0429cbb290ec5cda37ace
fujimura <ffujimurah@gmail.com>
2011-03-04 17:18:04 +0900

    introduce hash_initializer

--- IntroduceClassAnnotation/example.rb
+++ IntroduceClassAnnotation/example.rb
@@ -1,11 +1,16 @@
 class SearchCriteria
   attr_accessor :author_id, :publisher_id, :isbn
 
-  def initialize(hash)
-    @author_id = hash[:author_id]
-    @publisher_id = hash[:publisher_id]
-    @isbn = hash[:isbn]
+  def self.hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
   end
+
+  hash_initializer :author_id, :publisher_id, :isbn
 
 end
 
