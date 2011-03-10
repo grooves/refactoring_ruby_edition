83b04e8ebb1e75da72b9004f71c60413438cf3a1
chin <s.chin@grooves.co.jp>
2011-03-10 18:57:32 +0900

    use module

--- chapter6/IntroduceNamedParameter/sample1.rb
+++ chapter6/IntroduceNamedParameter/sample1.rb
@@ -1,10 +1,18 @@
-class SearchCriteria
-
-	def initialize(hash)
-		@author_id = hash[:author_id]
-		@publisher_id = hash[:publisher_id]
-		@isbn = hash[:isbn]
+module CustomInitializers
+	def hash_initializer(*attribute_names)
+   define_method(:initialize) do |*args|
+		 data = args.first || {}
+		 attribute_names.each do |attribute_name| 
+			 instance_variable_set "@#{attribute_name}", data[attribute_name]
+		 end
+	 end
 	end
+end
+
+Class.send :include, CustomInitializers
+
+class SearchCriteria
+hash_initializer :author_id, :publisher_id, :isbn
 
 	def puts_params
 		p "author_id => #{@author_id}"
