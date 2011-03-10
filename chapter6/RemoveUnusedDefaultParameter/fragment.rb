3ffed93cfff5235d50d64350fe47b31fbf9e3e11
chin <s.chin@grooves.co.jp>
2011-03-10 19:58:47 +0900

    add fragment for Removeunuseddefaultparameter

---
+++ chapter6/RemoveUnusedDefaultParameter/fragment.rb
@@ -0,0 +1,10 @@
+def product_count_items(search_criteria=nil)
+	criteria = search_criteria | @search_criteria
+	ProductCountItem.find_all_by_criteria(criteria)
+end
+
+↓
+
+def product_count_items(search_criteria)
+	ProductCountItem.find_all_by_criteria(search_criteria)
+end
