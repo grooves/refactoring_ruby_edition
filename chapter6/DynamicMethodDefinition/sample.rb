a1f8417880b972124ce7367074b1b43131a46c5a
chin <s.chin@grooves.co.jp>
2011-03-10 20:15:18 +0900

    define def_each

--- chapter6/DynamicMethodDefinition/sample.rb
+++ chapter6/DynamicMethodDefinition/sample.rb
@@ -1,12 +1,19 @@
+class Class
+	def def_each(*method_names, &block)
+		method_names.each do |method_name|
+			define_method method_name do
+				instance_exec method_name, &block
+			end
+		end
+	end
+end
+
 class Test
 	attr_accessor :state
 
-	[:failure, :error, :success].each do |method|
-		define_method method do
-			self.state = method
-		end
+	def_each :failure, :error do |method_name|
+		self.state = method_name
 	end
-
 end
 
 test = Test.new
