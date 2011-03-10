8c0d72aa0edd9f985941453b36ef85e0dec990ad
chin <s.chin@grooves.co.jp>
2011-03-10 18:44:40 +0900

    add test code

--- chapter6/IntroduceNamedParameter/sample1.rb
+++ chapter6/IntroduceNamedParameter/sample1.rb
@@ -6,4 +6,14 @@ class SearchCriteria
 		@publisher_id = publisher_id
 		@isbn = isbn
 	end
+
+	def puts_params
+		p "author_id => #{@author_id}"
+		p "publisher_id => #{@publisher_id}"
+		p "isbn => #{@isbn}"
+	end
 end
+
+
+s = SearchCriteria.new(1,2,'test')
+s.puts_params
