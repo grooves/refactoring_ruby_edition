8dbcc9533e50f8fb91eafe4180f3b3ebffccde28
chin <s.chin@grooves.co.jp>
2011-03-10 19:21:55 +0900

    convert conditions, joins params to a hash

--- chapter6/IntroduceNamedParameter/sample2.rb
+++ chapter6/IntroduceNamedParameter/sample2.rb
@@ -1,13 +1,15 @@
 class Books
-	def self.find(selector, conditions="", *joins)
+	def self.find(selector, hash={})
+		hash[:joins] ||= []
+		hash[:conditions] ||= ""
 		sql = ["select * from books "]
-		joins.each do |join_table|
+		hash[:joins].each do |join_table|
 			sql << "left outer join #{join_table} on "
 			#sql << "books,#{join_table.to_s.chap}_id"
 			sql << "books.#{join_table.to_s}_id"
 			sql << " = #{join_table}.id "
 		end
-		sql << "where #{conditions}" unless conditions.empty?
+		sql << "where #{hash[:conditions]}" unless hash[:conditions].empty?
 		sql << " limit 1" if selector == :first
     
 		#connection.find(sql.join(" "))
