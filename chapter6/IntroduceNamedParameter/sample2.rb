f25f47d2efd51fbe32ef09bd4d11695322f37400
chin <s.chin@grooves.co.jp>
2011-03-10 19:16:34 +0900

    make it more like sql

--- chapter6/IntroduceNamedParameter/sample2.rb
+++ chapter6/IntroduceNamedParameter/sample2.rb
@@ -1,14 +1,14 @@
 class Books
 	def self.find(selector, conditions="", *joins)
-		sql = ["select * from books"]
+		sql = ["select * from books "]
 		joins.each do |join_table|
-			sql << "left outer join #{join_table} on"
+			sql << "left outer join #{join_table} on "
 			#sql << "books,#{join_table.to_s.chap}_id"
-			sql << "books,#{join_table.to_s}_id"
-			sql << " = #{join_table}.id"
+			sql << "books.#{join_table.to_s}_id"
+			sql << " = #{join_table}.id "
 		end
 		sql << "where #{conditions}" unless conditions.empty?
-		sql << "limit 1" if selector == :first
+		sql << " limit 1" if selector == :first
     
 		#connection.find(sql.join(" "))
 
