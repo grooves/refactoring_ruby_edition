4025d1542352ea18ab5b83730db0abf420aeae26
chin <s.chin@grooves.co.jp>
2011-03-10 19:13:21 +0900

    add test code

--- chapter6/IntroduceNamedParameter/sample2.rb
+++ chapter6/IntroduceNamedParameter/sample2.rb
@@ -3,12 +3,20 @@ class Books
 		sql = ["select * from books"]
 		joins.each do |join_table|
 			sql << "left outer join #{join_table} on"
-			sql << "books,#{join_table.to_s.chap}_id"
+			#sql << "books,#{join_table.to_s.chap}_id"
+			sql << "books,#{join_table.to_s}_id"
 			sql << " = #{join_table}.id"
 		end
 		sql << "where #{conditions}" unless conditions.empty?
 		sql << "limit 1" if selector == :first
     
-		connection.find(sql.join(" ")
+		#connection.find(sql.join(" "))
+
+		p "sql: #{sql}"
 	end
 end
+
+Books.find(:all)
+Books.find(:all, "title like '%Voodoo Economics'")
+Books.find(:all, "authors.name = 'Jenny James'", :authors)
+Books.find(:first, "authors.name = 'Jenny James'", :authors)
