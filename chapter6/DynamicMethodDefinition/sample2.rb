2501a66f425a881542a9833ced8646078e9a1b1a
chin <s.chin@grooves.co.jp>
2011-03-10 20:35:35 +0900

    define to_module for Hash

--- chapter6/DynamicMethodDefinition/sample2.rb
+++ chapter6/DynamicMethodDefinition/sample2.rb
@@ -1,13 +1,21 @@
-class Test
-	def initialize(post_data)
-		(class << self; self; end).class_eval do
-			post_data.each_pair do |key, value|
-				define_method key.to_sym do
+class Hash
+	def to_module
+		hash = self
+		Module.new do
+			hash.each_pair do |key, value|
+				define_method key do
 					value
 				end
 			end
 		end
+	end
 end
+
+
+class Test
+	def initialize(post_data)
+		self.extend post_data.to_module
+	end
 end
 
 post_data = {:params => 'Paramas', :session => 'Session'}
